ea5e90b51e7960eb7e83314724ecb4d24c5c5e33
William Finocchiaro <89465648+WILLIAMFINOCCHIARO@users.noreply.github.com>
2021-11-28 17:51:29 +0100

    almost

--- js/script.js
+++ js/script.js
@@ -279,4 +279,18 @@
 // A lunedi!!
 
 
+const root = new Vue ({
+  el:"#root",
+  data:{
+    images : [
+      "img/01.jpg",
+      "img/02.jpg",
+      "img/03.jpg",
+      "img/04.jpg",
+      "img/05.jpg"
+    ]
 
+  },
+
+
+});
